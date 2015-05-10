52b798a69ef63aac80a76bb1728c867e08599072
Daida Medina <daida.ms@gmail.com>
2015-05-10 21:05:23 +0100

    Roman numerals should convert to VII when 7 is provided.

--- romanNumerals.js
+++ romanNumerals.js
@@ -2,8 +2,9 @@ var romanNumerals = {};
 
 romanNumerals.convertFrom = function(arabicNumber){
     var romanNumber = "";
-    if (arabicNumber === 5) {
-        return "V";
+    if (arabicNumber >= 5) {
+        romanNumber += "V";
+        arabicNumber -= 5;
     }
     for (var i = 0; i < arabicNumber; i++) {
         romanNumber += "I";
--- test/romanNumeralsShould.js
+++ test/romanNumeralsShould.js
@@ -4,17 +4,21 @@ var romanNumerals = require('../romanNumerals.js');
   describe('Roman Numerals', function(){
       it('should convert to I when 1 is provided', function(){
           romanNumerals.convertFrom(1).should.be.exactly("I");
-      })
+      });
 
       it('should convert to II when 2 is provided', function(){
           romanNumerals.convertFrom(2).should.be.exactly("II");
-      })
+      });
 
       it('should convert to III when 3 is provided', function(){
           romanNumerals.convertFrom(3).should.be.exactly("III");
-      })
+      });
 
       it('should convert to V when 5 is provided', function(){
           romanNumerals.convertFrom(5).should.be.exactly("V");
-      })
+      });
+
+      it('should convert to V when 7 is provided', function(){
+          romanNumerals.convertFrom(7).should.be.exactly("VII");
+      });
   })
