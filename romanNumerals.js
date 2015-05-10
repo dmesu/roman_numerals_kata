528e9dae8330fcadeb5fc97e54eaf49667584e08
Daida Medina <daida.ms@gmail.com>
2015-05-10 20:26:33 +0100

    Roman numerals should convert to II when 2 is provided.

--- romanNumerals.js
+++ romanNumerals.js
@@ -4,6 +4,9 @@ romanNumerals.convertFrom = function(arabicNumber){
     if (arabicNumber === 1){
         return "I"
     }
+    if (arabicNumber === 2){
+        return "II"
+    }
     return undefined;
 };
 
--- test/romanNumeralsShould.js
+++ test/romanNumeralsShould.js
@@ -3,7 +3,11 @@ var romanNumerals = require('../romanNumerals.js');
 
   describe('Roman Numerals', function(){
 
-    it('should return I when 1 is provided', function(){
+    it('should convert to I when 1 is provided', function(){
         romanNumerals.convertFrom(1).should.be.exactly("I");
+    })
+
+    it('should convert to II when 2 is provided', function(){
+        romanNumerals.convertFrom(2).should.be.exactly("II");
     })
   })
